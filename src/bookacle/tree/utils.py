1195b07cb6145168cbdac0b76b3a6a1a18b4177b
Malay Agarwal <malay.agarwal261016@outlook.com>
2024-09-04 00:14:29 +0530

    Remove split_documents utility functions

--- src/bookacle/tree/utils.py
+++ src/bookacle/tree/utils.py
@@ -2,36 +2,10 @@ from threading import Lock
 
 from bookacle.models import EmbeddingModelLike, SummarizationModelLike
 from bookacle.tree.structures import Node
-from langchain_core.documents import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from transformers import PreTrainedTokenizerBase
 
 
 def concatenate_node_texts(nodes: list[Node]) -> str:
     return "\n\n".join(" ".join(node.text.splitlines()) for node in nodes) + "\n\n"
-
-
-def split_documents(
-    documents: list[Document],
-    tokenizer: PreTrainedTokenizerBase,
-    max_tokens: int = 100,
-    overlap: int = 0,
-) -> list[Document]:
-    splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-        tokenizer=tokenizer,
-        chunk_size=max_tokens,
-        chunk_overlap=overlap,
-        separators=[
-            "\n\n",
-            "\n",
-            ".",
-            "!",
-            "?",
-        ],
-        keep_separator="end",
-    )
-
-    return splitter.split_documents(documents=documents)
 
 
 def create_parent_node(
