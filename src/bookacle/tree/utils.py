78c5c5b1c5680bff95f3c7bea086b8827829fa10
Malay Agarwal <malay.agarwal261016@outlook.com>
2024-09-03 23:18:13 +0530

    Move helper functions to a utils module

---
+++ src/bookacle/tree/utils.py
@@ -0,0 +1,57 @@
+from threading import Lock
+
+from bookacle.models import EmbeddingModel, SummarizationModel
+from bookacle.tree.structures import Node
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from transformers import PreTrainedTokenizerBase
+
+
+def concatenate_node_texts(nodes: list[Node]) -> str:
+    return "\n\n".join(" ".join(node.text.splitlines()) for node in nodes) + "\n\n"
+
+
+def split_documents(
+    documents: list[Document],
+    tokenizer: PreTrainedTokenizerBase,
+    max_tokens: int = 100,
+    overlap: int = 0,
+) -> list[Document]:
+    splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        tokenizer=tokenizer,
+        chunk_size=max_tokens,
+        chunk_overlap=overlap,
+        separators=[
+            "\n\n",
+            "\n",
+            ".",
+            "!",
+            "?",
+        ],
+        keep_separator="end",
+    )
+
+    return splitter.split_documents(documents=documents)
+
+
+def create_parent_node(
+    cluster: list[Node],
+    embedding_model: EmbeddingModel,
+    summarization_model: SummarizationModel,
+    new_level_nodes: dict[int, Node],
+    next_node_index: int,
+    summarization_length: int,
+    lock: Lock,
+) -> Node:
+    concatenated_text = concatenate_node_texts(nodes=cluster)
+
+    summary = summarization_model.summarize(
+        text=concatenated_text, max_tokens=summarization_length
+    )
+
+    return Node.from_text(
+        index=next_node_index,
+        text=summary,  # type: ignore
+        embedding_model=embedding_model,
+        children_indices={node.index for node in cluster},
+    )
