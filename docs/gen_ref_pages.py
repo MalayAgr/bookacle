aa1d5d98cd3f2571bae33d37970888302b36b09c
Malay Agarwal <malay.agarwal261016@outlook.com>
2024-09-30 02:05:34 +0530

    Improve automatic reference generation

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -4,11 +4,14 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
-nav = mkdocs_gen_files.Nav()  # type: ignore
+nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("src").rglob("*.py")):
-    module_path = path.relative_to("src").with_suffix("")
-    doc_path = path.relative_to("src").with_suffix(".md")
+root = Path(__file__).parent.parent
+src = root / "src"
+
+for path in sorted(src.rglob("*.py")):
+    module_path = path.relative_to(src).with_suffix("")
+    doc_path = path.relative_to(src).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
@@ -17,7 +20,7 @@ for path in sorted(Path("src").rglob("*.py")):
         parts = parts[:-1]
         doc_path = doc_path.with_name("index.md")
         full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__" or parts[-1] == "main" or parts[-1] == "logger":
+    elif parts[-1] == "__main__":
         continue
 
     nav[parts] = doc_path.as_posix()
@@ -26,7 +29,7 @@ for path in sorted(Path("src").rglob("*.py")):
         ident = ".".join(parts)
         fd.write(f"::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path)
+    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
