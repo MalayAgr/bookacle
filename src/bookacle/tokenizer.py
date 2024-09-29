95885efe5d9ce624a276476f25319340746b2659
Malay Agarwal <malay.agarwal261016@outlook.com>
2024-09-30 02:26:22 +0530

    Add docs for tokenizer

--- src/bookacle/tokenizer.py
+++ src/bookacle/tokenizer.py
@@ -2,8 +2,12 @@ from typing import Protocol
 
 
 class TokenizerLike(Protocol):
-    """A protocol that defines the methods that a tokenizer should implement."""
+    """A protocol that all tokenizers should follow."""
 
     def encode(self, *args, **kwargs) -> list[int]:  # type: ignore
-        """Encode the input into a list of integers."""
+        """Tokenize the input text into a list of integers.
+
+        Returns:
+            Tokenized input.
+        """
         ...
